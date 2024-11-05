7463583daa60eda3bf8fca9a603fb394325b93fb
end front <chris@hotmail.com>
2024-11-05 18:44:25 +0800

    feat: 埋点对象封装

--- aliyun-sls.init.ts
+++ aliyun-sls.init.ts
@@ -4,18 +4,38 @@ import SlsTracker from '@aliyun-sls/web-track-browser'
 //通过STS可以获取自定义时效和访问权限的临时身份凭证，无需开启Logstore的WebTracking功能，不会产生脏数据
 import createStsPlugin from '@aliyun-sls/web-sts-plugin'
 
-enum eventType {
-  evt_page_view,
+interface Navigator {
+  oscpu?: string
+  connection?: { effectiveType: string }
+}
+
+enum eventTypeEnum {
+  evt_console_error = 'evt_console_error',
+  evt_page_view = 'evt_page_view',
+  evt_button_click = 'evt_button_click',
+  evt_device_info = 'evt_device_info',
+  evt_performance = 'evt_performance',
+  evt_visit_source = 'evt_visit_source',
+  evt_share = 'evt_share',
+}
+
+// interface BaseInfo {
+//   projectName: string //唯一值
+// }
+interface UserData {
+  user_id: string | number
 }
 
 interface logDataType {
-  eventType: eventType
+  eventType: eventTypeEnum
+  [key: string]: unknown
 }
 
 declare global {
   interface Window {
     tracker: SlsTracker
-    logReport: (data: logDataType, immediate: boolean) => void //日志上报通用方法
+    LOGREPORT: LogReport
+    logReport(data: logDataType | Array<logDataType>, immediate: boolean)
   }
 }
 
@@ -26,9 +46,11 @@ const opts = {
   time: 10, // 发送日志的时间间隔，默认是10秒
   count: 10, // 发送日志的数量大小，默认是10条
   topic: 'topic', // 自定义日志主题
-  source: 'source', //日志来源。您可以自定义该字段，以便于识别。
+  source: 'Analysis', //日志来源。您可以自定义该字段，以便于识别。此处取项目的项目名字。唯一值
+  //日志标签信息。您可以自定义该字段，便于识别。
   tags: {
-    tags: 'tags', //日志标签信息。您可以自定义该字段，便于识别。
+    sdk_version: '1.0.0',
+    track_version: '1.0.0',
   },
 }
 
@@ -70,12 +92,86 @@ tracker.useStsPlugin(stsPlugin)
 
 window.tracker = tracker
 
-window.logReport = function (data: logDataType | Array<logDataType>, immediate: boolean = false) {
-  //立即上传
-  if (immediate) {
-    if (Array.isArray(data)) return tracker.sendBatchLogsImmediate(data)
-    return tracker.sendImmediate(data)
+class LogReport {
+  static userData: UserData = { user_id: 1 }
+  public setUserInfo(data: UserData) {
+    LogReport.userData = { ...LogReport.userData, ...data }
+    this.pageInit()
   }
-  if (Array.isArray(data)) return tracker.sendBatchLogs(data)
-  return tracker.send(data)
+
+  public pageInit() {
+    const [platform] = navigator.userAgent.match(/(?<=\()(.+?)(?=\))/g) as RegExpMatchArray
+    const [performanceNavigationTiming] = window.performance.getEntriesByType(
+      'navigation',
+    ) as Array<PerformanceNavigationTiming>
+
+    let resourceTimeTotal = 0
+    const timeOrigin = performance.timeOrigin
+    const resourceTimingArray = (
+      window.performance.getEntriesByType('resource') as Array<PerformanceResourceTiming>
+    ).map(item => {
+      resourceTimeTotal += item.duration
+      return {
+        resource_name: item.name.match(/\/[^/]*$/)?.[0],
+        encodedBodySize: item.encodedBodySize,
+        initiatorType: item.initiatorType,
+        loadStart: timeOrigin + item.startTime,
+        loadEnd: timeOrigin + item.responseEnd,
+        loadTime: item.duration,
+      }
+    })
+    this.logReport([
+      {
+        eventType: eventTypeEnum.evt_page_view,
+        key_url: location.href,
+        key_referrer: document.referrer,
+      },
+      {
+        eventType: eventTypeEnum.evt_device_info,
+        key_url: location.href,
+        key_referrer: document.referrer,
+        key_vendor: navigator.vendor || 'Firefox', //设备制造商
+        key_system: platform || (navigator as Navigator)?.oscpu, //操作系统
+        key_systemVersion: platform || (navigator as Navigator)?.oscpu, //操作系统版本
+        key_screenHeight: screen.height,
+        key_screenWidth: screen.width,
+        key_connectionType: (navigator as Navigator).connection?.effectiveType, //手机上网类型
+        key_language: navigator.language, //语言
+      },
+      {
+        eventType: eventTypeEnum.evt_performance,
+        //或者 performanceNavigationTiming.loadEventEnd - performanceNavigationTiming.startTime 值和duration一样
+        key_pageloadstart: timeOrigin + performanceNavigationTiming.startTime,
+        key_pageloadend: timeOrigin + performanceNavigationTiming.loadEventEnd,
+        key_resourceloadTime: resourceTimeTotal,
+        key_resourceArray: resourceTimingArray,
+      },
+      {
+        eventType: eventTypeEnum.evt_visit_source,
+        //或者 performanceNavigationTiming.loadEventEnd - performanceNavigationTiming.startTime 值和duration一样
+        // key_pageloadstart: timeOrigin + performanceNavigationTiming.startTime,
+        // key_pageloadend: timeOrigin + performanceNavigationTiming.loadEventEnd,
+        // key_resourceloadTime: resourceTimeTotal,
+        // key_resourceArray: resourceTimingArray,
+      },
+    ])
+  }
+
+  public logReport(data: logDataType | Array<logDataType>, immediate: boolean = false) {
+    //批量上报
+    if (Array.isArray(data)) {
+      data = data.map(item => {
+        item.user_id = LogReport.userData.user_id
+        return item
+      })
+      return immediate ? tracker.sendBatchLogsImmediate(data) : tracker.sendBatchLogs(data)
+    }
+    //单个上报
+    data = { ...data, user_id: LogReport.userData.user_id }
+    return immediate ? tracker.sendImmediate(data) : tracker.send(data)
+  }
 }
+const LOGREPORT = new LogReport()
+
+window.LOGREPORT = LOGREPORT
+window.logReport = LOGREPORT.logReport
